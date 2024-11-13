79779dd789fec5b1d7c63efe96f5326545a0fa1f
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-11-13 18:58:50 -0500

    editar cuenta por cobrar totalmente funcional

--- AppDjango/manejador_facturacion/urls.py
+++ AppDjango/manejador_facturacion/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import listar_facturas
+from .views import listar_facturas, listado_pagos_estudiante, pagar
 
+app_name = 'manejador_facturacion'  # Esto es necesario para el namespace
 urlpatterns = [
     path('', listar_facturas, name='listar_facturas'),
+    path('conceptos_pago/<int:estudiante_id>/', listado_pagos_estudiante, name='listado_conceptos_pago'),
+    path('pagar/', pagar, name='pagar'),
 ]
--- AppDjango/manejador_reportes/urls.py
+++ AppDjango/manejador_reportes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import generar_reporte, home, generar_cartera
 
+app_name = 'manejador_reportes'  # Esto es necesario para el namespace
 urlpatterns = [
     path('', generar_reporte, name='generar_reporte'),
     path('', home, name='home'),  
